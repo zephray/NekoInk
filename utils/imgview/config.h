18138d5dca7f5ef875cb0b795fc909a6c150f270
Wenting Zhang <zephray@outlook.com>
2022-06-13 20:09:58 -0400

    util: fix dithering for color panel; implement blue noise and ordered dithering

--- utils/imgview/config.h
+++ utils/imgview/config.h
@@ -11,8 +11,8 @@
 #if defined(BUILD_PC_SIM)
 #define TITLE "IMGVIEW"
 #define TARGET_FPS (30)
-#define DISP_WIDTH (2232)
-#define DISP_HEIGHT (1680)
+#define DISP_WIDTH (1448)
+#define DISP_HEIGHT (1072)
 #elif defined(BUILD_NEKOINK)
 #define DISP_WIDTH (2232)
 #define DISP_HEIGHT (1680)
@@ -24,16 +24,19 @@
 
 #ifdef ENABLE_COLOR
 // Options only applies if COLOR is enabled
-#define ENABLE_LPF // Enable LPF to avoid jagged edges
+//#define ENABLE_LPF // Enable LPF to avoid jagged edges
 //#define ENABLE_BRIGHTEN // Copy component to neighbor pixels, only valid on SIM
 #endif
 
-//#define DEPTH_1BPP // monochrome
+#define DEPTH_1BPP // monochrome
 //#define DEPTH_2BPP // 4 grey / 64 color
-#define DEPTH_4BPP // 16 grey / 4096 color
+//#define DEPTH_4BPP // 16 grey / 4096 color
 //#define DEPTH_8BPP
 
-#define ENABLE_DITHERING
+// Define either one of them to enable dithering
+#define DITHERING_ERROR_DIFFUSION
+//#define DITHERING_ORDERED
+//#define DITHERING_BLUE_NOISE
 
 // Dithering options
 #ifdef ENABLE_COLOR
